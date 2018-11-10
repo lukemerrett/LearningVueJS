a3a0767df6dbafb4c02c78cee51ed5e43d723372
Luke Merrett <lukeamerrett@gmail.com>
2018-11-10 20:59:43 +0000

    Showed usage of Vue components

--- 01_GettingStarted/index.js
+++ 01_GettingStarted/index.js
@@ -1,17 +1,29 @@
 let app = {};
 
+Vue.component("hello-message", {
+    props: ["name"],
+    template: "<div>{{name}}</div>"
+})
+Vue.component("shopping-item", {
+    props: ["item"],
+    template: "<li>{{item.name}}</li>"
+})
+
 window.onload = function() {
     app = new Vue({
         el: '#app',
         data: {
+            name: "Luke",
             welcome_message: 'Hello Vue',
             hover_msg: 'You loaded this page on ' + new Date().toLocaleString(),
             seen: true,
             todos: [
-                { text: "Buy milk" },
-                { text: "Buy eggs" },
-                { text: "Buy flour" },
                 { text: "Make pancakes!" }
+            ],
+            shopping_list: [
+                { id: 0, name: "Buy milk" },
+                { id: 1, name: "Buy eggs" },
+                { id: 2, name: "Buy flour" }
             ]
         },
         methods: {
