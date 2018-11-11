22af8b7a4df24ecd931eecc3a8c12dc4cf143251
Luke Merrett <lukeamerrett@gmail.com>
2018-11-11 09:41:20 +0000

    Added quick start template

---
+++ 00_BootstrappingTemplate/index.js
@@ -0,0 +1,13 @@
+let app = {};
+
+window.onload = function() {
+    app = new Vue({
+        el: '#app',
+        data: {
+            
+        },
+        methods: {
+            
+        }
+    })
+}
